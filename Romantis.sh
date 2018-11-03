1b914fd3dad4baa16f2e4b811b117476a158af80
Rezzabodong <42667691+Rezzabodong@users.noreply.github.com>
2018-11-03 07:15:42 +0000

    Create Romantis.sh

---
+++ Romantis.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#version 1.0
+#Variables
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+black='\E[0m'
+black='\033[0m'
+red='\e[1;34m'
+red='\e[1;36m'
+red='\e[1;31m'
+echo ""
+echo ""
+echo " ______         _"|lolcat
+echo " | (_) |       | |"|lolcat
+echo "    | __   __  | |"|lolcat
+echo "  _ |/  \_/  \_|/"|lolcat
+echo "  (_/ \__/ \__/ |__/"|lolcat
+echo "  , __"|lolcat
+echo " /|/  \                                   0"|lolcat
+echo "  |___/  __   _  _  _    __,   _  _  _|_     ,"|lolcat
+echo "  | \   /  \_/ |/ |/ |  /  |  / |/ |  |  |  / \_"|lolcat
+echo "  |  \_/\__/   |  |  |_/\_/|_/  |  |_/|_/|_/ \/ "|lolcat
+echo ""
+echo ""
+echo "==================================="|lolcat
+echo "             ╭Author╮   "|lolcat
+echo "             ╰Rezzaa╯"|lolcat
+echo " Facebook : Rezza Priatna"|lolcat
+echo " WhatsApp : 085885105039"|lolcat
+echo " Gmail    : rezabodong123@gmail.com"|lolcat
+echo " Telegram : @Gantas"|lolcat
+echo " Channel  : @CryptoIndoo"|lolcat
+echo "===================================="|lolcat
+echo ""
+echo ""
+echo ""
+echo ""
+echo "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_"|lolcat
+echo " KLIK ANGKA 1 DAN ENTER UNTUK MELANJUTKAN"
+echo "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_"|lolcat
+read -p "Massukan=>" pil;
+
+if [ $pil = 1 ] || [ $pil = 01 ]
+then
+clear
+echo " TUNGGU SEBENTAR "
+pkg install wget
+wget https://raw.githubusercontent.com/Rezzabodong/B-ToolsV3/master/nembak.sh
+sh nembak.sh
+fi
